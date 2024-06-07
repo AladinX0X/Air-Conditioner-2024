c02af9a8628c2f378dc4e28b86d984a8da248ed1
AladinX0X <121769504+AladinX0X@users.noreply.github.com>
2024-06-07 12:43:27 +0200

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -23,28 +23,6 @@ const updateGraph = (newData) => {
     });
 }
 
-
-
-document.getElementById("loginButton").addEventListener("click", function () {
-    const username = prompt("Enter username:");
-
-    const passwordInputType = prompt("Enter password (Click OK to toggle visibility)", "", "password");
-    const password = passwordInputType.value;
-
-    if (passwordInputType.type === "text") {
-        const toggleVisibility = confirm("Hide password?");
-        if (toggleVisibility) {
-            passwordInputType.type = "password";
-        }
-    }
-
-    if (username === "user" && password === "test123") {
-        alert("Login successful!");
-    } else {
-        alert("Incorrect username or password.");
-    }
-});
-
 document.getElementById("toggleThemeButton").addEventListener("click", function() {
     document.body.classList.toggle('dark-mode');
 });
@@ -100,15 +78,4 @@ document.getElementById("aboutButton").addEventListener("click", function () {
 
 document.getElementById("contactButton").addEventListener("click", function () {
     showModal("contactModal");
-});
-
-document.getElementById("loginButton").addEventListener("click", function () {
-    const username = prompt("Enter username:");
-    const password = prompt("Enter password:");
-
-    if (username === "user" && password === "test123") {
-        alert("Login successful!");
-    } else {
-        alert("Incorrect username or password.");
-    }
 });
